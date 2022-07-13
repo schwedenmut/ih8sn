21928bed31605de5ff0f8bdfabb2a20f1e39153f
Marc Schwede <9077622+schwedenmut@users.noreply.github.com>
2022-07-13 17:43:04 +0200

    Added add flag for missing properties

--- main.cpp
+++ main.cpp
@@ -133,11 +133,11 @@ int main(int argc, char *argv[]) {
 
     if (is_boot_completed_stage) {
         property_override("ro.boot.flash.locked", "1");
-        property_override("ro.boot.vbmeta.device_state", "locked");
+        property_override("ro.boot.vbmeta.device_state", "locked", true);
         property_override("ro.boot.verifiedbootstate", "green");
-        property_override("ro.boot.veritymode", "enforcing");
-        property_override("ro.boot.warranty_bit", "0");
-        property_override("ro.warranty_bit", "0");
+        property_override("ro.boot.veritymode", "enforcing", true);
+        property_override("ro.boot.warranty_bit", "0", true);
+        property_override("ro.warranty_bit", "0", true);
     }
 
     return 0;
